a7498dff122598636315e2601814f2a479c9e95b
hflores-qa <62617562+hflores-qa@users.noreply.github.com>
2026-03-14 09:56:47 -0600

    Update SandraTest.java

--- src/test/java/tests/SandraTest.java
+++ src/test/java/tests/SandraTest.java
@@ -1,9 +1,18 @@
 package tests;
 
-public class SandraTest extends BaseTest {
-    public void test1() {
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-@Test
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SandraTest extends BaseTest {
+
+    @Test
     @Order(2)
     @Tag("USER")
     @Story("Obtener usuario por ID")
@@ -18,8 +27,5 @@ public class SandraTest extends BaseTest {
                 .body("userId",equalTo(1))
                 .log();
         Allure.step("Validación de userId=1 completada");
-    }
-
-        
     }
 }
